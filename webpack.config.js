28c77fef053c99b3c192fe832c5ae428f6a7112c
HeRoMo <hero@asterisk-works.jp>
2017-10-06 06:38:36 +0900

    improve webpack, babel setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  devtool: 'source-map',
+  externals: [nodeExternals()],
   module: {
     loaders: [{
       test: /\.js$/,
