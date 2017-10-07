8dc50c93d7bc158bd4f32ae011c569c01f89c071
HeRoMo <hero@asterisk-works.jp>
2017-10-07 18:00:18 +0900

    Add SNS Topic

--- handler.js
+++ handler.js
@@ -1,22 +1,8 @@
 'use strict';
 
-// module.exports.hello = async (event, context, callback) => {
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: 'Go Serverless v1.0! Your function executed successfully!',
-//       input: event,
-//     }),
-//   };
-//
-//   callback(null, response);
-//
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-// };
-//
-
 export const hello = async (event, context, callback) => {
+  console.log(JSON.stringify(event))
+  console.log(JSON.stringify(JSON.parse(event.Records[0].Sns.Message)))
   const response = {
     statusCode: 200,
     body: JSON.stringify({
