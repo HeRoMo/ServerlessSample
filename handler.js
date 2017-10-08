7ae9e6a7ff339df9d44328858095419c510de288
HeRoMo <hero@asterisk-works.jp>
2017-10-08 09:06:42 +0900

    Add CONFIG YAML file

--- handler.js
+++ handler.js
@@ -1,8 +1,26 @@
 'use strict';
 
+import Github from 'github-api';
+
 export const hello = async (event, context, callback) => {
-  console.log(JSON.stringify(event))
-  console.log(JSON.stringify(JSON.parse(event.Records[0].Sns.Message)))
+  if(event.Records && event.Records.length > 0 ){
+    console.log(JSON.stringify(event))
+    const message = JSON.parse(event.Records[0].Sns.Message)
+    console.log(JSON.stringify(message))
+
+    const gh = new Github({
+      token: process.env.GITHUB_TOKEN
+    });
+    const repo = gh.getRepo('HeRoMo','Test')
+    const opts = {
+      state: 'pending'
+    }
+    const commitSHA = message.pull_request.head.sha
+    repo.updateStatus(commitSHA, opts).then(function(data){
+      console.log(data)
+    })
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
