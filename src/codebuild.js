d42c54c401ea9fd93913c77d900e776d659ac9c6
HeRoMo <hero@asterisk-works.jp>
2017-10-09 17:06:01 +0900

    add codebuild result handling

--- src/codebuild.js
+++ src/codebuild.js
@@ -1,11 +1,38 @@
+import Github from './lib/github.js'
 
 export const dispatch = async (event, context, callback) => {
-
   console.log("EVENT: %s", JSON.stringify(event));
+
+  const buildStatus = event.detail['build-status']
+  const currentPhase = event.detail['current-phase']
+  const info = event.detail['additional-information']
+  const commitSHA = info['source-version']
+  const githubOpts = { context: 'TESTING' }
+
+  switch(buildStatus){
+    case 'IN_PROGRESS':
+      // NO-OP
+      break;
+    case 'SUCCEEDED':
+      githubOpts.state: 'success'
+      githubOpts.description: 'ユニットテストOK'
+      break;
+    case 'FAILED':
+      githubOpts.state: 'failure'
+      githubOpts.description: 'ユニットテストNG'
+      // currentPhase:'BUILD' 以外ではNGでなくエラーとしたほうが良いかもしれない
+      break;
+    case 'STOPPED':
+      githubOpts.state: 'pending'
+      githubOpts.description: 'ユニットテストが停止されました'
+      break;
+  }
+  await Github.updatePRStatus(commitSHA, githubOpts);
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'pull_request.js executed successfully!',
+      message: 'codebuild.js was executed successfully!',
       input: event,
     }),
   };
