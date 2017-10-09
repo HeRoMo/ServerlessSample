35a88d23272a9e1832e7703c2d34e1b3b473d01b
HeRoMo <hero@asterisk-works.jp>
2017-10-09 10:08:06 +0900

    refactering

--- src/handler.js
+++ src/handler.js
@@ -4,29 +4,43 @@ import Github from 'github-api';
 import slack from 'slack-incoming-webhook'
 import AWS from 'aws-sdk';
 const codebuild = new AWS.CodeBuild();
+const lambda = new AWS.Lambda();
+
+function parseEvent(event){
+  const githubEventType = event.Records[0].Sns.MessageAttributes["X-Github-Event"].Value
+  const message = JSON.parse(event.Records[0].Sns.Message)
+  const pullRequest = message.pull_request
+  const pullRequestAction = message.action
+  console.log("message: %s", JSON.stringify(message))
+  return {
+    githubEventType,
+    pullRequestAction,
+    pullRequest
+  }
+}
 
 export const dispatch = async (event, context, callback) => {
-  if(event.Records && event.Records.length > 0 ){
-    const githubEventType = event.Records[0].Sns.MessageAttributes["X-Github-Event"].Value
-    const message = JSON.parse(event.Records[0].Sns.Message)
-    const pullRequest = message.pull_request
-    const pullRequestAction = message.action
-    console.log("githubEventType: %s, pullRequestAction: %s", githubEventType, pullRequestAction)
-    console.log(JSON.stringify(message))
-    await updatePRStatus(pullRequest)
-    const codebuildParams = {
-      projectName: process.env.CODEBUILD_PROJECT,
-      buildspecOverride: process.env.CODEBUILD_BUILDSPEC,
-      sourceVersion: pullRequest.head.sha
-    }
-    codebuild.startBuild(codebuildParams, (err, data)=>{
-      if(err){
-        console.log(err, err.stack)
-      } else {
-        console.log(JSON.stringify(data))
-      }
-    })
-    await sendMessageToSlack("UNIT TEST START ")
+  if (!event.Records || event.Records.length <= 0){
+    context.callbackWaitsForEmptyEventLoop = false
+    callback(null, { message: "dispatch function is called by EMPTY event" });
+    return
+  }
+
+  const {githubEventType, pullRequestAction, pullRequest} = parseEvent(event)
+  console.log("githubEventType: %s, pullRequestAction: %s", githubEventType, pullRequestAction)
+
+  switch(pullRequestAction){
+    case 'opened':
+    case 'synchronize':
+      await updatePRStatus(pullRequest);
+      await startUnitTest(pullRequest);
+      await sendMessageToSlack("UNIT TEST START")
+      break;
+    case 'closed':
+      // ステージングの破棄
+      break;
+    default:
+      // NO-OP
   }
   const response = {
     statusCode: 200,
@@ -38,6 +52,25 @@ export const dispatch = async (event, context, callback) => {
 
   callback(null, response);
 };
+
+function startUnitTest(pullRequest){
+  const codebuildParams = {
+    projectName: process.env.CODEBUILD_PROJECT,
+    buildspecOverride: process.env.CODEBUILD_BUILDSPEC,
+    sourceVersion: pullRequest.head.sha
+  }
+  return new Promise(function(resolve, reject){
+    codebuild.startBuild(codebuildParams, (err, data)=>{
+      if(err){
+        console.log(err, err.stack)
+        reject(err);
+      } else {
+        console.log(JSON.stringify(data))
+        resolve(data)
+      }
+    });
+  })
+}
 
 async function updatePRStatus(pullRequest){
   const gh = new Github({
@@ -52,6 +85,7 @@ async function updatePRStatus(pullRequest){
   const commitSHA = pullRequest.head.sha
   const result = await repo.updateStatus(commitSHA, opts)
   console.log(JSON.stringify(result.data))
+  return result.data
 }
 
 async function sendMessageToSlack(message){
@@ -59,4 +93,18 @@ async function sendMessageToSlack(message){
     url: process.env.SLACK_WEBHOOK_URL
   }
   slack(message, opts)
+}
+
+function invokeLambda(payload){
+  const params = {
+    FunctionName: "my-service-dev-pull_request",
+    InvocationType: "Event",
+    Payload: JSON.stringify(payload)
+  };
+  return new Promise(function(resolve, reject){
+    lambda.invoke(params, function(err, data) {
+      if(err) { reject(err);
+      } else { resolve(data); }
+    })
+  })
 }
---
+++ src/pull_request.js
@@ -0,0 +1,13 @@
+
+export const synchronize = async (event, context, callback) => {
+
+  console.log("EVENT: %s", JSON.stringify(event));
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'pull_request.js executed successfully!',
+      input: event,
+    }),
+  };
+  callback(null, response);
+}
